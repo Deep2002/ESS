1c44dc6bb7489f433fdf5fae3a85f13a624a48ed
Deep Parmar <deepp4846@gmail.com>
2024-04-24 18:51:22 -0500

    first commit

---
+++ src/main/java/com/example/ess/EmailSendingService.java
@@ -0,0 +1,28 @@
+package com.example.ess;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSendingService {
+
+    private final JavaMailSender mailSender;
+
+    
+
+    @Autowired
+    public EmailSendingService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(String fromEmail, String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
---
+++ src/main/java/com/example/ess/EssController.java
@@ -0,0 +1,48 @@
+package com.example.ess;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+public class EssController {
+
+    private final String fromEmail = "shaktiflowentertainment@gmail.com";
+
+    private final EmailSendingService emailSendingService;
+
+    @Autowired
+    public EssController(EmailSendingService emailSendingService) {
+        this.emailSendingService = emailSendingService;
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/ess")
+    public Boolean sendEmail(@RequestBody Map<String, String> data) {
+        String name = data.get("name");
+        String email = data.get("email");
+        String phone = data.get("phone");
+        String eventType = data.get("eventType");
+        String numOfGuests = data.get("numOfGuests");
+        String eventDate = data.get("eventDate");
+        String eventTime = data.get("eventTime");
+
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + phone);
+        System.out.println("Event type: " + eventType);
+        System.out.println("# of Guests: " + numOfGuests);
+        System.out.println("Event Date: " + eventDate);
+        System.out.println("Event Time: " + eventTime);
+
+        emailSendingService.sendEmail(fromEmail, email, "Thank you - "+name, "Hi "+name+", Thank you for contacting us!\n\nWe will reach out to you soon!");
+        emailSendingService.sendEmail(fromEmail, email, name + " - New Event", "Event requested by: " + name);
+
+
+        return true;
+    }
+}
+ 
